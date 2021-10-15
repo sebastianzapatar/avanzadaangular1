5e01e5180af53a8dc36c02984fbde8ce8c44fc59
Sebastian Zapata <pansezapata@gmail.com>
2021-10-15 11:29:49 -0500

    se crea la aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'avanzada';
+  title = 'otro titulo';
+  edad=17;
+  estudiantes=['Sergio Camacho','Juan Santamaria'
+  ,'Lorena Lopez','Miller Paiva'];
+  products = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers lo mas de calidoso',
+      price: 80000,
+      description: 'bla bla bla bla bla1'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla2'
+    }
+  ];
 }
